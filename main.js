23b3a5e7518265de0ae0f478a1f5cc222883bee9
dann <dann_angeles@hotmail.com>
2024-10-08 20:59:38 -0600

    llenado de información

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    strings: ["Diseñadora Ux/Ui", "Desarrolladora Frontend", "Emprendedora"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
